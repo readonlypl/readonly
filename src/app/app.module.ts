2f8ccd264f10b3be5a464b88ede3b3fe1762c717
readonlypl <dev@readonly.pl>
2017-03-08 23:11:57 +0100

    main app template, configure sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
+import { UiModule } from './ui';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([]),
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
