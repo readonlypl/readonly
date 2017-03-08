3e55af7db761ea6af2624a0e0b7fb09ff46b4dfa
readonlypl <dev@readonly.pl>
2017-03-08 22:19:18 +0100

    initial module structure

---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SidebarComponent]
+})
+export class UiModule { }
