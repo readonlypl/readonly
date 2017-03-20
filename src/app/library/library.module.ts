13fee28767bcf9da3cdc14258b8c92c5055ac4dc
readonlypl <dev@readonly.pl>
2017-03-20 21:21:07 +0100

    Library model and repository

--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LibraryRoutes } from './library.routing';
 import { LibraryListComponent } from './library-list/library-list.component';
+import { LibraryRepository } from './library.repository';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     LibraryRoutes
   ],
   declarations: [
     LibraryListComponent
+  ],
+  providers: [
+    LibraryRepository
   ]
 })
 export class LibraryModule { }
---
+++ src/app/library/library.repository.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Library } from './library.model';
+import { environment } from './../../environments/environment';
+
+@Injectable()
+export class LibraryRepository {
+
+  constructor(
+    private http: Http
+  ) {}
+
+
+  public findAll(): Observable<Array<Library>> {
+    return this.http.get(environment.apiUrl + 'libraries')
+      .map((response: Response): Array<Library> => {
+        return response.json()
+          .map((library) => new Library(library.id, library.name));
+      });
+  }
+
+  public create(library: Library) {
+    return this.http.post(
+      environment.apiUrl + 'libraries',
+      library
+    );
+  }
+
+}
